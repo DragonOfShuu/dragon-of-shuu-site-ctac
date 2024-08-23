df6c43428bf907ff6b816b2db92481c0c2bd3fb9
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2024-08-23 01:36:16 -0600

    Plans & NavBar

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -2,6 +2,7 @@ import HamburgerMenuIcon from "@/assets/lineIcons/hamburgerMenu.svg";
 import Link from "next/link";
 import LineIconButton from "@/components/LineIconButton";
 import NavLink from "./NavLink";
+import styles from './NavBar.module.sass'
 
 type Props = {};
 
@@ -24,14 +25,14 @@ const NavBar = (props: Props) => {
 
     return (
         <nav className={`fixed top-0 left-0 right-0 z-50`}>
-            <div className={`flex items-center h-14 px-3 py-2`}>
+            <div className={`flex items-center lg:grid grid-flow-col lg:grid-cols-3 h-14 px-3 py-2`}>
                 <h1
                     className={`text-3xl text-amber-500 text-glow shadow-amber-500`}
                 >
                     <Link href={`/`}>Dragon of Shuu</Link>
                 </h1>
 
-                <div className={`ml-4 gap-2 hidden md:flex`}>
+                <div className={`ml-4 gap-2 hidden md:flex justify-end lg:justify-center flex-grow`}>
                     {navLinks.map((l) => (
                         <NavLink
                             href={l.href}
@@ -41,13 +42,13 @@ const NavBar = (props: Props) => {
                         />
                     ))}
                 </div>
-
-                <div className={`grow`} />
-
-                <LineIconButton
-                    svg={HamburgerMenuIcon}
-                    className={`h-full md:hidden`}
-                />
+                
+                <div className={`md:hidden flex justify-end h-full flex-grow`}>
+                    <LineIconButton
+                        svg={HamburgerMenuIcon}
+                        className={`w-14`}
+                    />
+                </div>
             </div>
         </nav>
     );
