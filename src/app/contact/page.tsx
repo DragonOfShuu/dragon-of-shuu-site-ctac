0248d6ef58745d722b514622bd35c149fd9446b2
Logan Cederlof <68718280+DragonOfShuu@users.noreply.github.com>
2024-08-27 22:58:20 -0600

    Added Submit Form Button

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,6 @@
 import EpicForm from "@/components/epicForms/EpicForm";
 import EpicFormRow from "@/components/epicForms/EpicFormRow";
+import EpicFormSubmit from "@/components/epicForms/EpicFormSubmit";
 import PageHeader from "@/components/PageHeader";
 
 const ContactUs = () => {
@@ -11,14 +12,15 @@ const ContactUs = () => {
             <div className="w-full flex flex-col items-center">
                 <EpicForm method={`get`} action={'/contact/thankYou'} className={`w-full lg:w-[900px]`}>
                     <EpicFormRow displayname={`Name`} paramName={`name`}>
-                        <input id={`name`} type={`text`} placeholder={`John Doe`} />
+                        <input id={`name`} name={`name`} type={`text`} placeholder={`John Doe`} />
                     </EpicFormRow>
                     <EpicFormRow displayname={`Message`} paramName={`message`}>
-                        <textarea id={`message`} placeholder={`Message...`} className={`w-full`} />
+                        <textarea id={`message`} name={`message`} placeholder={`Message...`} className={`w-full h-48`} />
                     </EpicFormRow>
                     <EpicFormRow displayname={`Return Email`} paramName={`ret_addr`}>
-                        <input id={`ret_addr`} placeholder={`johndoe@example.com`} type={`email`} />
+                        <input id={`ret_addr`} name={`ret_addr`} placeholder={`johndoe@example.com`} type={`email`} />
                     </EpicFormRow>
+                    <EpicFormSubmit />
                 </EpicForm>
             </div>
         </>
---
+++ src/components/epicForms/EpicFormSubmit.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import SpecialButton from "../SpecialButton";
+import useEpicFormData from "./contexts/EpicFormContext";
+import EpicFormLabelless from "./EpicFormLabelless"
+
+type EpicFormSubmitPropsType = {
+    submitText?: string,
+}
+
+const EpicFormSubmit = (props: EpicFormSubmitPropsType) => {
+    const {epicFormData} = useEpicFormData();
+
+    const errors = epicFormData.errors;
+    const buttonDisabled = Object.keys(errors).length!==0
+
+    return (
+        <EpicFormLabelless>
+            <SpecialButton type="submit" disabled={buttonDisabled}>
+                {props.submitText??'Submit'}
+            </SpecialButton>
+        </EpicFormLabelless>
+    )
+}
+
+export default EpicFormSubmit;
