f3167f15d2431bb4f34876e1bd361aefda5ecbb2
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2025-01-05 00:31:21 -0700

    Projects page is more responsive

--- src/app/projects/ProjectItem.tsx
+++ src/app/projects/ProjectItem.tsx
@@ -20,31 +20,29 @@ const ProjectItem = (props: ProjectDisplayPropType) => {
 
     return (
         <div className={`${styles.item}`} onClick={() => setOpen((o) => !o)}>
-            <div className={`${styles.text}`}>
-                <h1>{props.name}</h1>
-                <div className={`flex gap-1 justify-center md:justify-start`}>
-                    {props.tags?.sort().map((tagName) => {
-                        const colors = props.tagColorsTable[tagName] ?? [
-                            180, 180, 180,
-                        ];
-                        const colorString = colors.join(", ");
-                        return (
-                            <div
-                                className={`${styles.tag}`}
-                                style={{
-                                    backgroundColor: `rgb(${colorString})`,
-                                }}
-                                key={tagName}
-                            >
-                                {tagName}
-                            </div>
-                        );
-                    })}
-                </div>
-                <Markdown className={`${open ? `block` : `hidden`}`}>
-                    {props.description}
-                </Markdown>
+            <h1 className={`text-3xl ${styles.text}`}>{props.name}</h1>
+            <div className={`flex gap-1 justify-center md:justify-start flex-wrap`}>
+                {props.tags?.sort().map((tagName) => {
+                    const colors = props.tagColorsTable[tagName] ?? [
+                        180, 180, 180,
+                    ];
+                    const colorString = colors.join(", ");
+                    return (
+                        <div
+                            className={`${styles.tag}`}
+                            style={{
+                                backgroundColor: `rgb(${colorString})`,
+                            }}
+                            key={tagName}
+                        >
+                            {tagName}
+                        </div>
+                    );
+                })}
             </div>
+            <Markdown className={`${open ? `block` : `hidden`} ${styles.text}`}>
+                {props.description}
+            </Markdown>
             <div className={`${styles.interactables}`}>
                 <Link href={props.href} target={props.href.startsWith('/') ? `_self` : `_blank`}>
                     <SpecialButton className={`shadow-md shadow-gray-900/50`}>
@@ -52,6 +50,8 @@ const ProjectItem = (props: ProjectDisplayPropType) => {
                     </SpecialButton>
                 </Link>
             </div>
+
+            {/* Background elements */}
             <div className="absolute inset-0 -z-10 bg-gradient-to-r from-amber-950 via-orange-900 via-40%" />
             {props.image.height && props.image.width ? (
                 <div className="absolute inset-0 -z-20">
